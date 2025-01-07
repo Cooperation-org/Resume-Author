12b066b59c460044476770ba4135e516ce57b54a
Omar Salah <omar.salah1597@gmail.com>
2025-01-07 18:20:56 +0200

    feat: integrate Redux and Auth0 for state management and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import Home from './pages/home'
-import { Provider } from 'react-redux'
-import { Auth0Provider } from '@auth0/auth0-react'
-import store from './redux/store'
 import Resume from './pages/resume'
 import ImportPage from './pages/importPage'
 import Error404 from './pages/error404'
 import LandingPage from './pages/allskillscoun-org'
 import './styles/App.css'
+import { fetchVCs } from './redux/slices/vc'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from './redux/store'
 
 const App = () => {
+  const dispatch: AppDispatch = useDispatch()
+
+  // Fetch VCs when the app loads
+  useEffect(() => {
+    dispatch(fetchVCs())
+  }, [dispatch])
   return (
-    <Provider store={store}>
-      <Auth0Provider
-        domain='dev-z2863ou7zzmrmnxc.us.auth0.com'
-        clientId='PbToefw6udJDXLf0Eyf1sX3u0oqE5Dzq'
-        authorizationParams={{
-          redirect_uri: `${window.location.origin}/resume/import`,
-        }}
-      >
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/resume/new' element={<Resume />} />
-            <Route path='/resume/import' element={<ImportPage />} />
-            <Route path='/landing' element={<LandingPage />} />
-            <Route path='*' element={<Error404 />} />
-          </Routes>
-        </Router>
-      </Auth0Provider>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/resume/new' element={<Resume />} />
+        <Route path='/resume/import' element={<ImportPage />} />
+        <Route path='/landing' element={<LandingPage />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+    </Router>
   )
 }
 
