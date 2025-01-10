0f0428f4f3d611383a6f15c14a39f5ecb749dc1f
Omar Salah <omar.salah1597@gmail.com>
2025-01-10 20:38:48 +0200

    Feat: integrate VC slice and enhance ResumeEditor components with tooltip support

--- src/components/ResumeEditor/LeftSidebar.tsx
+++ src/components/ResumeEditor/LeftSidebar.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import { Box, Paper, Typography } from '@mui/material'
 import SectionContent from './SectionContent' // Import the reusable SectionContent component
 
+interface LeftSidebarProps {
+  highlightedText: string
+  credentials: string[]
+  tooltipPosition: { top: number; left: number } | null
+}
+
 export const leftSections: (keyof Resume)[] = ['contact', 'languages']
 
-const LeftSidebar = () => {
+const LeftSidebar: React.FC<LeftSidebarProps> = ({
+  highlightedText,
+  credentials,
+  tooltipPosition
+}) => {
   return (
     <Box sx={{ width: 240 }}>
       <Typography variant='h6' mb={2}>
@@ -12,8 +22,12 @@ const LeftSidebar = () => {
       </Typography>
       {leftSections.map(sectionId => (
         <Paper key={sectionId} sx={{ mb: 2, p: 2, borderRadius: 2 }}>
-          {/* Use SectionContent for each section */}
-          <SectionContent sectionId={sectionId} />
+          {/* Use SectionContent for each section and pass tooltip props */}
+          <SectionContent
+            sectionId={sectionId}
+            highlightedText={highlightedText}
+            tooltipPosition={tooltipPosition}
+          />
         </Paper>
       ))}
     </Box>
