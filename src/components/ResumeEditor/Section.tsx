19b57f119e2c3b53659c7ee825502ba8f44b93ff
Omar Salah <omar.salah1597@gmail.com>
2025-01-10 20:38:48 +0200

    Feat: integrate VC slice and enhance ResumeEditor components with tooltip support

--- src/components/ResumeEditor/Section.tsx
+++ src/components/ResumeEditor/Section.tsx
@@ -3,12 +3,18 @@ import SectionContent from './SectionContent'
 
 interface SectionProps {
   sectionId: keyof Resume
+  highlightedText: string
+  tooltipPosition: { top: number; left: number } | null
 }
 
-const Section = ({ sectionId }: SectionProps) => {
+const Section = ({ sectionId, highlightedText, tooltipPosition }: SectionProps) => {
   return (
     <Paper sx={{ p: 3, mb: 3, borderRadius: 2, boxShadow: 1 }}>
-      <SectionContent sectionId={sectionId} />
+      <SectionContent
+        sectionId={sectionId}
+        highlightedText={highlightedText}
+        tooltipPosition={tooltipPosition}
+      />
     </Paper>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Auth0Provider
+        domain='dev-z2863ou7zzmrmnxc.us.auth0.com'
+        clientId='PbToefw6udJDXLf0Eyf1sX3u0oqE5Dzq'
+        authorizationParams={{
+          redirect_uri: `${window.location.origin}/resume/import`
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </Provider>
   </React.StrictMode>
 )
 
