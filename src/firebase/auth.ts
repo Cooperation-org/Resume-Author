fbf8938c97051edc4b595289104a9adb0003c029
Omar Salah <omar.salah1597@gmail.com>
2025-01-27 21:34:40 +0200

    refactor: update Firebase configuration to use environment variables and enhance Google sign-in flow

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,28 +1,35 @@
 import { auth, googleProvider } from '../config/firebase'
 import { signInWithPopup, signOut, User } from 'firebase/auth'
 import { setCookie, setLocalStorage } from '../tools'
-import { getAccessToken } from './storage'
+import { GoogleAuthProvider } from 'firebase/auth'
 
 export const signInWithGoogle = async (): Promise<User | null> => {
   try {
+    googleProvider.addScope('https://www.googleapis.com/auth/userinfo.profile')
+    googleProvider.addScope('https://www.googleapis.com/auth/userinfo.email')
+    googleProvider.addScope('https://www.googleapis.com/auth/drive.file')
+
     const result = await signInWithPopup(auth, googleProvider)
+
     if (!result.user) {
       throw new Error('No user found')
     }
+
+    const credential = GoogleAuthProvider.credentialFromResult(result)
+    const accessToken = credential?.accessToken
+    if (!accessToken) {
+      throw new Error('No access token found')
+    }
+
     setCookie('refresh_token', result.user.refreshToken, {
       secure: true,
       sameSite: 'strict',
       expires: 7
     })
-    const accessToken = await getAccessToken(result.user.uid)
-    console.log('🚀 ~ signInWithGoogle ~ accessToken:', accessToken)
-    if (accessToken) {
-      setCookie('access_token', accessToken, {
-        secure: true,
-        sameSite: 'strict',
-        expires: 7
-      })
-    }
+
+    console.log('Access Token:', accessToken)
+    setCookie('accessToken', accessToken, {})
+
     setLocalStorage('user', JSON.stringify(result.user))
 
     return result.user
