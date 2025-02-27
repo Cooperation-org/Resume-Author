4046c304737e43aa139f996377eb3b8c879e2b12
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2025-02-27 23:39:12 +0200

    the flow of the sign up page

--- src/pages/signUpStep2.tsx
+++ src/pages/signUpStep2.tsx
@@ -1,14 +1,13 @@
 import { Box, Typography, Button } from '@mui/material'
 import { SVGLogoDescreption, SVGALoginLogo, SVGQRCode } from '../assets/svgs'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { login } from '../tools/auth'
 
 export default function SignUpStep2() {
-  const location = useLocation()
   const navigate = useNavigate()
 
   const handleGoogleSignIn = () => {
-    login(location.pathname)
+    login('/resume/import')
   }
 
   return (
